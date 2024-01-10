ac28369848c53f6b88986f4dcfa5775d7630b76f
Colby <colby.braden@gmail.com>
2024-01-09 20:41:09 -0600

    updated fish config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,4 +1,5 @@
 alias v="nvim"
+alias weather="curl wttr.in"
 alias sway="dbus-launch --exit-with-session sway"
 #alias sway="dbus-run-session sway --unsupported-gpu -D noscanout"
 alias reboot="loginctl reboot"
